8de572c509e39747583dd7ace526c5895f9d3712
Nathan Seymour <nathan@seymour.global>
2025-02-07 16:26:37 -0600

    update city version;

--- test/basic.c
+++ test/basic.c
@@ -3,9 +3,28 @@ int constant()
     return 34;
 }
 
+void other_routine()
+{
+    //int x = 34 + 2;
+    return;
+}
+
 int __entry()
 {
-    int x = constant();
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int e = 5;
+    int f = 6;
 
-    return x + 2;
+    constant();
+
+    int subtotal = a + b - c;
+
+    other_routine();
+
+    int total = subtotal + d - e + f;
+
+    return total;
 }
